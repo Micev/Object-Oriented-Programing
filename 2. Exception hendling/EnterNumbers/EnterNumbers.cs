12ae158f63f56b7a00a6b4603717ffa0a34776b0
micev <micevgeorgi@gmail.com>
2014-11-22 14:28:41 +0200

    add home work

---
+++ 2. Exception hendling/EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterNumbers
+{
+    class EnterNumbers
+    {
+        public static int ReadNumber(int start, int end)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+            if (number <= start)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (number >= end)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return number;
+        }
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int end = 100;
+            int counter = 0;
+            while (counter < 10)
+            {               
+                try
+                {
+                    int number = ReadNumber(start, end);
+                    start = number;
+                    counter++;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("Number must be > form {0}",start);
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Number must be < form {0}", end);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Number");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Number is to Big!!!");
+                }
+            }
+        }
+    }
+}
---
+++ 2. Exception hendling/Square Root/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number=0;
+        double sqrt;
+        try
+        {
+            number = int.Parse(Console.ReadLine());
+            if (number < 0) throw new ArgumentOutOfRangeException();
+            else
+            {
+                sqrt = Math.Sqrt(number);
+                Console.WriteLine(sqrt);
+            }
+        }
+        catch(FormatException)
+        {
+            Console.WriteLine("Invalid Number");
+        }
+        catch(OverflowException)
+        {   
+            Console.WriteLine("Invalid Number");
+        }
+        catch(ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Invalid Number");
+        }
+        finally
+        {
+            Console.WriteLine("Good Bye");
+        }
+    }
+}
+
